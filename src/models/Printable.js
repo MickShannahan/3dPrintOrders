f1d42eb109babfd0f3e1b6f772aae1714812f9ac
mick shannahan <beasticorn@gmail.com>
2023-08-05 16:26:55 -0600

    bleh paths

--- src/models/Printable.js
+++ src/models/Printable.js
@@ -8,7 +8,7 @@ export class printable {
   constructor(name, picture, cost) {
     this.id = uuid()
     this.name = name
-    this.picture = '/Previews/' + picture
+    this.picture = picture
     this.cost = cost
   }
 }

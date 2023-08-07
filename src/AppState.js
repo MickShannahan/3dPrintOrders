00f63e0900a18eb5e59ee73cd627d91b95e4826c
mick shannahan <beasticorn@gmail.com>
2023-08-06 18:04:29 -0600

    bunch of vases

--- src/AppState.js
+++ src/AppState.js
@@ -17,9 +17,16 @@ export const AppState = reactive({
     new printable('Rex Dragon', 'RexDragon.png', 10),
     new printable('Oslo', 'Oslo.png', 15),
     new printable('Bingus', 'Bingus.png', 15),
+    new printable('Geometric Succulent Planter', 'Geometric Succulant Planter.png', 10),
+    new printable('Large Spiral Planter', 'Large Spiral Planter.png', 15),
+    new printable('Lined Planter', 'Lined Planter.png', 15),
+    new printable('Oval Succulent Planter', 'Oval Succulant Planter.png', 10),
+    new printable('Spiral Planter', 'Spiral Planter.png', 15),
+    new printable('Waffle Planter', 'Waffle Planter.png', 15),
+    new printable('Waffle Succulent Planter', 'Waffle Succulant Planter.png', 10),
     new printable('Slim Body Vase', 'Body Vase.png', 20),
     new printable('Natural Body Vase', 'Natural Body Vase.png', 20),
-    new printable('Sitting Vase', 'Sitting Vase.png', 20),
+    new printable('Sitting Body Vase', 'Sitting Vase.png', 20),
     new printable('Jigglypuff', 'Jiggly.png', 10)
   ],
   colors: [
